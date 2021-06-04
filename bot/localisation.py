99bbfebf2c43a7876a1b5169391c132f837c7f16
sampathsir <83348785+sampathsir@users.noreply.github.com>
2021-06-04 11:07:16 +0530

    Update localisation.py

--- bot/localisation.py
+++ bot/localisation.py
@@ -22,7 +22,7 @@ class Localisation:
     
     RCHD_TG_API_LIMIT = "Downloaded in {} seconds.\nDetected File Size: {}\nSorry. But, I cannot upload files greater than 1.95GB due to Telegram API limitations."
     
-    COMPRESS_SUCCESS = "📥 Downloaded in {}\n\n📀 Compressed in {}\n\n📤 Uploaded in {}\n\nBy @AyushBots"
+    COMPRESS_SUCCESS = "📥 Downloaded in {}\n\n📀 Compressed in {}\n\n📤 Uploaded in {}\n\nBy @dengerousbots"
 
     COMPRESS_PROGRESS = "⏳ ETA: {}\n🚀 Progress: {}%"
 
